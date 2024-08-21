6547ecbd2fadddac4af7c34e6b55189e4bb45c95
PavinSP <pavinsp122002@gmail.com>
2024-08-22 05:11:51 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,23 @@
 import java.util.Scanner;
-class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int k = scanner.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             arr[i] = scanner.nextInt();
         }
-        if (arr.length == 1) {
-            System.out.println(-1);
-        } else {
-            int max_count = 0;
-            int max_count_element = Integer.MIN_VALUE;
-            boolean tie = false;
-            for (int i = 0; i < n; i++) {
-                int count = 0;
-                for (int j = 0; j < n; j++) {
-                    if (arr[i] == arr[j]) {
-                        count++;
-                    }
-                }
-                if (count > max_count) {
-                    max_count = count;
-                    max_count_element = arr[i];
-                    tie = false;
-                } else if (count == max_count && max_count_element != arr[i]) {
-                    tie = true;
-                }
+        int max = 0;
+        for (int i = 1; i <= n - k + 1; i++){
+            int current = 0;
+            for (int j = 1; j < k + 1; j++){
+                current += (j * arr[i + j - 2]);
             }
-            if (tie) {
-                System.out.println(-1);
-            } else {
-                System.out.println(max_count_element);
+            if (current > max){
+                max = current;
             }
         }
+        System.out.println(max);
     }
 }
--- src/Problem1.java
+++ src/Problem1.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class Problem1 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
+        int n = scanner.nextInt(); // no of elements in an array (no of shots made)
+        int k = scanner.nextInt(); // size of the contiguous sub array
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++){
             arr[i] = scanner.nextInt();
         }
         int max_score = 0;
-        for (int i = 0; i <= n-k; i++){
-            int current_score = 0;
+        for (int i = 1; i <= n-k + 1; i++){ // n - k + 1 (no of possible sub arrays, ex: [1, 2, 3, 4, 5] and k = 2, so possible sub arrays are [1, 2], [2, 3], [3, 4], [4, 5]
+            int current_score = 0; // score for each sub array
             for (int j = 1; j < k+1; j++){
-                current_score += j * arr[i+j-1];
+                current_score += j * arr[i+j-2];
             }
-            max_score = Math.max(current_score, max_score);
+            max_score = Math.max(current_score, max_score); // max score updated
         }
         System.out.println(max_score);
     }
