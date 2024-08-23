835dd8757858ad864667b076c62e0b521b0419ba
PavinSP <pavinsp122002@gmail.com>
2024-08-23 18:02:59 +0530

    commit

--- src/Problem6_1.java
+++ src/Problem6_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Problem6{
+class Problem6_1 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
